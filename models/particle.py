747ec2451f4a8b7cde46df25d54f3e1c5ef4527b
Mikhail Zharov <mike.zharov91@gmail.com>
2025-12-01 02:33:23 +0300

    fix cumulativer analyzer

--- models/particle.py
+++ models/particle.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional
+import math
 
 
 @dataclass
@@ -17,3 +18,25 @@ class Particle:
     t_formed: float
     charge: Optional[int] = None
     baryon_density: Optional[float] = None
+
+    @property
+    def pt(self) -> float:
+        # transverse momentum
+        return math.hypot(self.px, self.py)
+
+    @property
+    def eta(self) -> Optional[float]:
+        # pseudorapidity from momentum
+        p2 = self.px * self.px + self.py * self.py + self.pz * self.pz
+        if p2 == 0.0:
+            return None
+        p = math.sqrt(p2)
+        # avoid division by zero when p == |pz|
+        denom = p - abs(self.pz)
+        if denom <= 1e-12:
+            return None
+        # standard eta definition: 0.5 * ln((p+pz)/(p-pz))
+        try:
+            return 0.5 * math.log((p + self.pz) / (p - self.pz))
+        except (ZeroDivisionError, ValueError):
+            return None
